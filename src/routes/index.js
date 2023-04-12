50ef2d2c260ba3efd9fa03fc651f170486860545
Jhonnata Nascimento da Paixão <114832000+jhownpaixao@users.noreply.github.com>
2023-04-11 23:42:33 -0300

    organização da sub rotas de mensagem (:session/message)

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,11 @@ const router = express.Router();
 router.post('/create-session', MainController.SessionAdd)
 
 router.post('/:session/start-session', MainController.SessionStart)
-router.post('/:session/send-text', MainController.SendText)
 router.get('/:session/check-connection-session', MainController.SessionStatus)
 router.get('/:session/validate-number/:number', MainController.ValidateNumber)
 router.get('/:session/disconnect', MainController.SessionDesconnect)
 router.get('/:session/delete', MainController.SessionDelete)
+router.post('/:session/message/send-text', MainController.SendText)
 
 
 module.exports = router;
